5191c592065c55748edbb25726aaf26578ee1e09
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-06 12:30:09 +0700

    membuat method traverse

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -89,4 +89,24 @@ public:
         return true;
     }
 
-    
+    void traverse()
+    {
+        if (listEmpty())
+        {
+            cout << "\nList kosong\n";
+        }
+        else
+        {
+            cout << "\nData didalam list adalah :\n";
+            Node *currentNode = START;
+            while (currentNode !=NULL)
+            {
+                cout << currentNode->noMhs << endl;
+                currentNode = currentNode->next;
+            }
+            cout << endl;
+        }
+    }
+};
+
+
