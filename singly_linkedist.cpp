c20d838bf1085b9b6fb3964948c3dcabba1777f2
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-06 12:21:52 +0700

    membuat kondisi IF

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -28,4 +28,15 @@ public:
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
 
-        
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if((START !=NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+        }
+    
+
