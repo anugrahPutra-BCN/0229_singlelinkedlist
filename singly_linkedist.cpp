fd369b3137d2f0e1356c934ce8345eb7f7ebf7c3
AnugrahPutra_BCN <anugrahputra@gmail.com>
2025-05-06 13:43:05 +0700

    memproses input user dan memanggil fungsi

--- singly_linkedist.cpp
+++ singly_linkedist.cpp
@@ -127,4 +127,68 @@ int main()
         cout << endl
              << "Masukkan pilihan (1-5): ";
         cin  >> ch;
+        switch (ch)
+        {
+        case '1':
+        {
+            mhs.addNode();
+        }
+            break;
         
+
+        case '2':
+        {
+            if (mhs.listEmpty())
+            {
+                cout << endl
+                     << "List Kosong" << endl;
+                break;
+            }
+            cout << endl
+                 << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if(mhs.delNode(nim) == false)
+        
+
+        cout << endl
+             << "\nMasukkan no mahasiswa yang akan dihapus : ";
+        cin >> nim;
+        if (mhs.delNode(nim) == false)
+           cout << "Data tidak ditemukan" << endl;
+        else 
+           cout << "Data dengan nomor mahasiswa " << nim << "berhasil dihapus " << endl;
+        }
+        break;
+        case '3':
+        {
+            mhs.traverse();
+        }
+        break;
+        case '4':
+        {
+            if (mhs.listEmpty() == true)
+            {
+                cout << "\nList kosong\n";
+                break;
+            }
+            Node *previous, *current;
+            cout << endl
+                 << "Masukkan no mahasiswa yang dicari : ";
+            cin >> nim;
+            if (mhs.search(nim, &previous, &current) == false)
+                cout << endl
+                     << "Data tidak ditemukan" << endl;
+            else
+            {
+                cout << endl 
+                     << "Data ditemukan" << endl;
+                cout << "\nNo mahasiswa: " << current->noMhs << endl;
+                cout << "\n";
+            }
+        }
+        break;
+        case '5':
+        {
+            exit(0);
+        }
+       
